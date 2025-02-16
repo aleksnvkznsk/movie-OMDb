d28d0523f87455e8fd3763d20a9c06e2a7038d31
aleksnvkznsk <aleksandrov.developer@gmail.com>
2025-02-16 12:19:18 +0700

    change intro

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -5,6 +5,7 @@ function Intro() {
         <div className="intro">
             <div className="intro__content">
                 <h1 className="intro__content__title">MOVIE OBDb</h1>
+                <p className="intro__content__description">Ваша база данных фильмов.  Ищите, оценивайте и делитесь своими любимыми кинолентами!</p>
                 <a className="intro__content__button" href='/movie'>Movie</a>
             </div>
         </div>
