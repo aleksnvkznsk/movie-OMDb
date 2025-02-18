bcc6dc02804ab6ce0b83f8430e2896f63596eb6f
aleksnvkznsk <aleksandrov.developer@gmail.com>
2025-02-18 09:05:32 +0700

    new header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,27 +1,37 @@
-import "./Header.scss";
+import { useState } from 'react';
+import './Header.scss';
 
-function Header() {
+const Header = () => {
     const NavLinks = [
         { href: "/", label: "Главная" },
         { href: "/movie", label: "Фильмы" },
         { href: "/featured", label: "Избранное" },
     ];
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <header className="header">
-            <nav className="header__nav">
-                <ul className="header__list">
-                    {NavLinks.map((link, index) => (
-                        <li key={index} className="header__item">
-                            <a href={link.href} className="header__link">
-                                {link.label}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
+            <button className="header__burger-button" onClick={toggleMenu}>
+                <span className="header__burger-line header__burger-line--top"></span>
+                <span className="header__burger-line header__burger-line--middle"></span>
+                <span className="header__burger-line header__burger-line--bottom"></span>
+            </button>
+
+            <nav className={`header__menu ${isOpen ? 'header__menu--open' : ''}`}>
+                {NavLinks.map((link, index) => (
+                    <li key={index} className="header__item">
+                        <a href={link.href} className="header__link">
+                            {link.label}
+                        </a>
+                    </li>
+                ))}
             </nav>
         </header>
     );
-}
+};
 
 export default Header;
